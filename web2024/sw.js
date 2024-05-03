447c17353fed375e7e59853ec5fae5abb514dafb
sieunhantrieu <thanhtrieu118@yahoo.com>
2024-05-03 14:40:13 +0700

    up

--- web2024/sw.js
+++ web2024/sw.js
@@ -8,3 +8,31 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+
+// thong bao quyen
+
+if ('Notification' in window) {
+  Notification.requestPermission().then(function(result) {
+    if (result === 'granted') {
+      console.log('Quyền thông báo đã được cấp!');
+    } else {
+      console.log('Quyền thông báo bị từ chối.');
+    }
+  });
+}
+
+// thong bao cai dat
+
+if ('Notification' in window) {
+  Notification.requestPermission().then(function(result) {
+    if (result === 'granted') {
+      var notification = new Notification('Cài đặt Siêu Nhân Triều !', {
+        body: 'Cài đặt Siêu Nhân Triều để truy cập nhanh chóng và ngoại tuyến.',
+        icon: 'ico/Icon-72x72.png'
+      });
+      notification.onclick = function() {
+        window.location.href = 'https://sieunhan.xyz'; // Thay thế bằng URL website của bạn
+      };
+    }
+  });
+}
